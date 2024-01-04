d53720fc64d574597270f4393d5dca1db45250dd
MrHelsinki <taha.chad2001@gmail.com>
2024-01-04 17:01:40 +0100

    auto push msg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     if len == 0:
         print("0 arguments.")
     elif len == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(len))
+        print("{} arguments:".format(len))
     
     for i in range(len):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
