7b3d34be50f5bae4eec70a6b9e4e893a50604d31
MrHelsinki <taha.chad2001@gmail.com>
2024-01-04 16:59:37 +0100

    auto push msg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    len = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(count))
+    
+    for i in range(len):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
