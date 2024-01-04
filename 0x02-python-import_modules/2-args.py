82804c60c6978d9f0d46679bc738a250be3bebd8
MrHelsinki <taha.chad2001@gmail.com>
2024-01-04 17:09:18 +0100

    auto push msg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-
-    len = len(argv) - 1
-    if len == 0:
-        print("0 arguments.")
-    elif len == 1:
-        print("1 argument:")
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
     else:
-        print("{} arguments:".format(len))
-    
-    for i in range(len):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
