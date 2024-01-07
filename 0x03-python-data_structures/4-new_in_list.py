f4fd2106a1c4292b4e7e58676f194b334104b226
MrHelsinki <taha.chad2001@gmail.com>
2024-01-08 00:14:20 +0100

    auto push msg

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     tmp = my_list.copy()
-    
+
     if idx < 0:
         return tmp
     if idx >= len(tmp):
         return tmp
-    
+
     tmp[idx] = element
     return tmp
