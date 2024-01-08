0385be2811ebef5ae0f38f76409f6cf0aca46adf
MrHelsinki <taha.chad2001@gmail.com>
2024-01-08 01:23:18 +0100

    auto push msg

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max = 0
+    maxi = 0
     for i in my_list:
-        if i > max:
-            max = i
-    return max
+        if i > maxi:
+            maxi = i
+    return maxi
