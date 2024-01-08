f20013361da68b22e905a52f592b42ba1fa6b3a7
MrHelsinki <taha.chad2001@gmail.com>
2024-01-08 01:32:31 +0100

    auto push msg

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     bool_list =[]
     for i in my_list:
-        if i % 2 = 0:
+        if i % 2 == 0:
             bool_list.append(True)
         else:
             bool_list.append(False)
