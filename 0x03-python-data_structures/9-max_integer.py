aff5f9cc94cd875013497fc01a4ce0c279564e2e
MrHelsinki <taha.chad2001@gmail.com>
2024-01-08 01:21:47 +0100

    auto push msg

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = 0
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
