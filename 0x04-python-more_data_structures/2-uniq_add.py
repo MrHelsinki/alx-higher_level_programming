cd79c75a3f68d819114b638a3be8ab2886d5d06a
MrHelsinki <taha.chad2001@gmail.com>
2024-01-10 23:50:26 +0100

    auto push msg

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,8 @@
 #!/user/bin/python3
 
 def uniq_add(my_list=[]):
-    uniq_list = list()
+    uniq_list = set(my_list)
     sum = 0
     for i in my_list:
-        for x in uniq_list:
-            if i == x:
-                break
-        uniq_list.append(i)
         sum += i
-
     return sum
