baae74111819004a12eb248280a9feef78d90b18
MrHelsinki <taha.chad2001@gmail.com>
2024-01-10 23:37:27 +0100

    auto push msg

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+
+def uniq_add(my_list=[]):
+    uniq_list = []
+    for i in list:
+        for x in uniq_list:
+            if i == x:
+                break
+        uniq_list.append(i)
+        sum += i
+
+    return sum
