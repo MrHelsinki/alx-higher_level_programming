211402cef769e2d538c64d8fc53011995a5e6c15
MrHelsinki <taha.chad2001@gmail.com>
2024-01-23 15:32:26 +0100

    auto push msg

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Square Square Square"""
+
+
+class Square:
+    """ Square class with init size
+    Attributes:
+        __size (int): size ... maybe ?
+    """
+
+    def __init__(self, size):
+        """ init the square
+        Args:
+            size (int): size ... probably
+        """
+        if type(size) is not (int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
