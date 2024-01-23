d2ac8cf1b3e196cac3075b686c8bbd2bad2b6a2e
MrHelsinki <taha.chad2001@gmail.com>
2024-01-23 15:22:04 +0100

    auto push msg

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,15 @@
 
 
 class Square:
-    """ Square class with init size"""
+    """ Square class with init size
+    Attributes:
+        __size (int): size ... maybe ?
+    """
+    
     def __init__(self, size):
-        self.size = size
+        """ init the square
+        Args:
+            size (int): size ... probably
+        """
+
+        self.__size = size
