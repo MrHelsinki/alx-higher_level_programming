eeb06da782229295da4ad4df364a5aa414d34999
MrHelsinki <taha.chad2001@gmail.com>
2024-01-23 15:39:06 +0100

    auto push msg

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,7 +14,8 @@ class Square:
         Args:
             size (int): size ... probably
         """
-        if type(size) is not (int):
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
