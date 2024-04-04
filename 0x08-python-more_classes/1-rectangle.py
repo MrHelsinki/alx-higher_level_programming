e256844be3a92a14b74658c8b57256341d04498b
MrHelsinki <taha.chad2001@gmail.com>
2024-04-04 10:29:41 +0000

    commit auto

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
 	""" define Rectangle"""
 	def __init__(self, width=0 , height=0):
 		""" set necessary attributes"""
-		self.__width = width
-		self.__height = height
+		self.width = width
+		self.height = height
 	
 	@property
 	def width(self):
