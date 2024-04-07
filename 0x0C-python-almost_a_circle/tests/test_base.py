5b2e6de93ec46534a11a70f322c1a56faf8b3879
MrHelsinki <taha.chad2001@gmail.com>
2024-04-07 04:30:41 +0000

    commit auto

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_noarg(self):
+        case_one = Base()
+        case_two = Base()
+        self.assertEqual(case_one.id, case_two.id - 1)
+    
